4640d1145d8f89711639a8b90fe1287fc8336376
Jared Norman <jared@creepywizard.com>
2016-07-05 12:38:19 -0700

    Add the formula

--- lib/games_back.rb
+++ lib/games_back.rb
@@ -3,9 +3,25 @@ require "games_back/version"
 module GamesBack
   class << self
     def calculate(records)
-      Hash[records.map do |id, record|
-        [id, nil]
-      end]
+      results = records.map do |player, record|
+        if player == 1
+          [player, nil]
+        else
+          [player, games_behind(records[1], record)]
+        end
+      end
+      Hash[results]
+    end
+
+    private
+
+    def games_behind(player_a, player_b)
+      player_a_wins = player_a[0]
+      player_a_losses = player_a[1]
+      player_b_wins = player_b[0]
+      player_b_losses = player_b[1]
+
+      (player_a_wins - player_b_wins + player_b_losses - player_a_losses) / 2.0
     end
   end
 end
