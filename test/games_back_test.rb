d9285b16d762a5f80707da43cf6e877184bfc7f5
Jared Norman <jared@creepywizard.com>
2016-07-05 12:22:20 -0700

    Add a failing test

--- test/games_back_test.rb
+++ test/games_back_test.rb
@@ -10,4 +10,10 @@ class GamesBackTest < Minitest::Test
     expected = { 1 => nil }
     assert_equal expected, results
   end
+
+  def test_it_works_for_two_rankings
+    results = GamesBack.calculate({ 1 => [2, 1], 2 => [1, 2] })
+    expected = { 1 => nil, 2 => 1 }
+    assert_equal expected, results
+  end
 end
